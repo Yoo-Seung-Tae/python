686d246878caffc5b3af03dfe53057c0d0157f3b
YOO <graycuracao@gmail.com>
2024-11-30 10:32:12 +0900

    again push

---
+++ 11_29.py
@@ -0,0 +1,21 @@
+'''
+ddddddddddd
+print("hello world")
+'''
+
+print("hello", "world", sep="__")
+print()
+print("hello", "world", sep="\n\n")
+print("hello", "world", end="")
+print("dddd")
+name="유"
+name2="승태"
+# print(f'내이름은 {name+name2}입니다.')
+
+
+num="dd"
+Num="aa"
+# print(num, Num)
+
+ive="장원영"
+print(f"아이브 {ive}")
